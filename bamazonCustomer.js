29e8b593dddbd9367d8eb9570717a87b5f3417a9
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-08-15 12:38:02 -0400

    bamazonCustomer: added inquirer npm, item() function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,14 +2,59 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "bamazon"
-});
+    host     : "localhost",
+    user     : "root",
+    password : "root",
+    database : "bamazon"
+  });
 
 connection.connect(function(err){
     if (err) throw err;
-    start();
-})
+    items();
+});
+
+function items() {
+    connection.query("SELECT * FROM ",function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++ ) {
+            console.log("Item ID: " + res[i].item_id + "\n", "Product: " + res[i].product_name + "\n", "Price: " + res[i].price + "\n");
+        }
+
+        inquirer
+            .prompt ([
+                {
+                     name: "choice",
+                     type: "rawlist",
+                     choices: function() {
+                        var choiceArray = [];
+                        for (var i = 0; i < res.length; i++) {
+                            choiceArray.push(res[i].item_id);
+                        }
+                        return itemList;
+                     },
+                     message: "What is the item_id of the product you would like to buy?"
+                },
+                {
+                name: "productUnits",
+                type: "input",
+                message: "How many units of this product do you wish to purchase?"
+                }
+            ])
+            .then(function(answer){
+                
+                var pickedItem;
+                for (var i = 0; i < res.length; i++) {
+                    if (res[i].item_id == answer.choice) {
+                        pickedItem = res[i];
+                        return pickedItem;
+                    }
+                }
+
+                if (pickedItem.stock_quantity < parseInt(answer.productUnits)) {
+                    console.log("Insufficient Quantity!");
+                    items();
+                }
+                
+            })
+    })
+};
--- bamazonSQL.sql
+++ bamazonSQL.sql
@@ -1,12 +1,49 @@
 DROP DATABASE IF EXISTS bamazon;
 CREATE DATABASE bamazon;
 
-USE bamazon
+USE bamazon;
 
 CREATE TABLE products (
-    item_id INT(2) NOT NULL PRIMARY KEY,
+    item_id INT(2) AUTO_INCREMENT NOT NULL PRIMARY KEY,
     product_name VARCHAR(30) NOT NULL,
     department_name VARCHAR(30),
     price INT NOT NULL,
     stock_quantity INT(3) NOT NULL
 );
+
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Guitar", "Music", 90, 10);
+    
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Wasteland, Baby!", "Music", 15, 150);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Crockpot", "Kitchenware", 50, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Comforter Set", "Home", 75, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Tablet", "Electronics", 70, 22);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES     ("Go Pro Hero7", "Electronics", 400, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES     ("Harry Potter Boxset", "Books", 54, 54);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES     ("CANON iP110 Mobile Printer", "Office Supplies", 150, 45);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES    ("Travel: Europe", "Books", 16, 30);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES     ("Dog Treats", "Pets", 7, 20);
+
+SELECT * FROM products;
