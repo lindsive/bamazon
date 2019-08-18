54f156ad3dc34fe48f5c9d2a2f4ba4fc35e44759
Lindsay Iveson <ivesonlindsay@gmail.com>
2019-08-18 00:24:42 -0400

    added connection.end() to end connection with mySQL

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,86 +16,82 @@ connection.connect(function (err) {
 
 function items() {
     connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
+            if (err) throw err;
+
             console.table(res);
+
+            inquirer
+                .prompt([{
+                        name: "pick",
+                        type: "rawlist",
+                        choices: function () {
+                            var itemList = [];
+                            for (var i = 0; i < res.length; i++) {
+                                itemList.push(res[i].product_name);
+                            }
+                            return itemList;
+                        },
+                        message: "What is the product you would like to buy?"
+                    },
+                    {
+                        name: "customerPurchase",
+                        type: "input",
+                        message: "How many units of this product do you wish to purchase?"
+                    }
+                ])
+                .then(function (answer) {
+
+                    if (isNaN(answer.customerPurchase)) {
+                        console.log("Please enter a numerical value");
+
+                        inquirer
+                            .prompt({
+                                name: "customerPurchase",
+                                type: "input",
+                                message: "How many units of this product do you wish to purchase?"
+                            })
+                    }
+
+                    connection.query(
+                        "SELECT * FROM products WHERE ?", {
+                            product_name: answer.pick
+                        },
+                        function (err, res) {
+                            if (err) throw err
+
+                            var stock = res[0].stock_quantity;
+
+                            var amountPurchased = answer.customerPurchase;
+
+                            if (stock >= amountPurchased) {
+                                var updateStock = stock - amountPurchased;
+
+                                console.log("Purchase successful!")
+                                console.log("There are now " + updateStock + " " + answer.pick + "s in stock");
+
+                                connection.query(
+                                    "UPDATE products SET ? WHERE ?", [{
+                                            stock_quantity: updateStock
+                                        },
+                                        {
+                                            product_name: answer.pick
+                                        }
+                                    ],
+                                    function (err) {
+                                        if (err) throw err;
+                                    }
+                                );
+
+                            } else if (stock <= amountPurchased && !NaN) {
+                                console.log("Insufficient Quantity")
+                            } else {
+                                connection.end()
+                            }
+
+                        },
+                    );
+                });
         }
 
-        inquirer
-            .prompt([{
-                    name: "pick",
-                    type: "rawlist",
-                    choices: function () {
-                        var itemList = [];
-                        for (var i = 0; i < res.length; i++) {
-                            itemList.push(res[i].product_name);
-                        }
-                        return itemList;
-                    },
-                    message: "What is the product you would like to buy?"
-                },
-                {
-                    name: "customerPurchase",
-                    type: "input",
-                    message: "How many units of this product do you wish to purchase?"
-                }
-            ])
-            .then(function (answer) {
-
-                if (isNaN(answer.customerPurchase)) {
-                    console.log("Please enter a numerical value");
-
-                    inquirer
-                        .prompt({
-                            name: "customerPurchase",
-                            type: "input",
-                            message: "How many units of this product do you wish to purchase?"
-                        })
-                }
-
-                connection.query(
-                    "SELECT * FROM products WHERE ?", {
-                        product_name: answer.pick
-                    },
-                    function (err, res) {
-                        if (err) throw err
-
-                        var stock = res[0].stock_quantity;
-
-                        var amountPurchased = answer.customerPurchase;
-
-                        if (stock >= amountPurchased) {
-                            var updateStock = stock - amountPurchased;
-
-                            console.log("Purchase successful!")
-                            console.log("There are now " + updateStock + " " + answer.pick + "s in stock");
-
-
-                            connection.query(
-                                "UPDATE products SET ? WHERE ?", [{
-                                        stock_quantity: updateStock
-                                    },
-                                    {
-                                        product_name: answer.pick
-                                    }
-                                ],
-                                function (err) {
-                                    if (err) throw err;
-                                }
-                            );
-
-                        } else if (stock <= amountPurchased && !NaN){
-                            console.log("Insufficient Quantity")
-                        }
-
-                    },
-                );
-
-
-            })
-    })
+    );
 };
-
-
-// minus stock of purchase and then u
-// stock in one var; customer input in anorther var
